576e1a3a9ea1cf671ff62b10ecdb7f2ab82a4a44
Travis Gritter <travis.gritter@gmail.com>
2020-02-16 18:56:20 -0800

    Fixed charts bug

--- src/variables/charts.js
+++ src/variables/charts.js
@@ -137,7 +137,7 @@ const emailsSubscriptionChart = {
 const completedTasksChart = {
   data: {
     labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-    series: [[230, 750, 450, 300, 280, 240, 200, 190][230, 750, 450, 300, 280, 240, 200, 190]]
+    series: [[230, 750, 450, 300, 280, 240, 200, 190]]
   },
   options: {
     lineSmooth: Chartist.Interpolation.cardinal({
